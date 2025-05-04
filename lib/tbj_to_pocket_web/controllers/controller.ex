7b9e69f42043c2a7434715d76133d63b96d0e9b5
benonymus <bence1204@gmail.com>
2025-05-04 18:58:45 +0700

    retry on 403s

--- lib/tbj_to_pocket_web/controllers/controller.ex
+++ lib/tbj_to_pocket_web/controllers/controller.ex
@@ -1,6 +1,5 @@
 defmodule TbjToPocketWeb.Controller do
   use TbjToPocketWeb, :controller
-
   require Logger
 
   def show(conn, %{"id" => id}) do
@@ -47,8 +46,15 @@ defmodule TbjToPocketWeb.Controller do
     access_token = Application.fetch_env!(:tbj_to_pocket, :pocket_access_token)
     url = "#{TbjToPocketWeb.Endpoint.url()}/articles/#{id}"
 
+    retry_func = fn _req, resp ->
+      case resp do
+        %{status: 200} -> false
+        _ -> true
+      end
+    end
+
     Req.post("https://getpocket.com/v3/add",
-      retry: :transient,
+      retry: retry_func,
       receive_timeout: 30_000,
       json: %{
         consumer_key: consumer_key,
